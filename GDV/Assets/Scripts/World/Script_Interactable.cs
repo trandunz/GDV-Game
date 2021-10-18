fe4d4117fc09eced9a5f1703ce83e7bb92165d9c
Trandunz <trandunz@gmail.com>
2021-10-19 12:07:54 +1300

    + Debug Log Interactable Workbench

---
+++ GDV/Assets/Scripts/World/Script_Interactable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Interactable : MonoBehaviour
+{
+    [SerializeField] string m_ObjectName;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag is "Player" && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.F)))
+        {
+            Debug.Log("Player Interacted With " + m_ObjectName + "!");
+        }
+    }
+}
