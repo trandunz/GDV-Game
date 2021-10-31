b79742b37cbb92ffd51e7c07d75503c4cbb24184
MichaelWkTran <Michael.wktran@gmail.com>
2021-10-31 13:14:10 +1300

    Updating crewmate and camera system
    Cinemachine and A* pathfinding from https://arongranberg.com/astar/ has been added which allows for dynamic camera and crewmate pathfinding

--- GDV/Assets/Scripts/CrewMate/Script_CrewMate.cs
+++ GDV/Assets/Scripts/CrewMate/Script_CrewMate.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Script_CrewMate : MonoBehaviour
 {
@@ -30,20 +31,54 @@ public class Script_CrewMate : MonoBehaviour
     [SerializeField] protected MATESTATE m_MateState = MATESTATE.UNASSIGNED;
     [SerializeField] protected MOOD m_Mood = MOOD.UNASSIGNED;
 
+    [SerializeField] protected float m_Speed;
+    [SerializeField] private Transform TargetPosition;
+
+    [SerializeField] protected float m_NextWaypointDistance;
+    [SerializeField] protected Path m_Path;
+    [SerializeField] protected int m_CurrentWaypoint = 0;
+    protected Seeker m_Seeker;
+
+    protected Rigidbody2D m_RigidBody;
     protected Animator m_Animator;
 
     void Start()
     {
-        if (gameObject.GetComponent<Animator>())
+        if (GetComponent<Rigidbody2D>())
         {
-            m_Animator = gameObject.GetComponent<Animator>();
+            m_RigidBody = GetComponent<Rigidbody2D>();
+        }
+        if (GetComponent<Animator>())
+        {
+            m_Animator = GetComponent<Animator>();
         }
 
         m_Mood = MOOD.CONTENT;
         m_MateState = MATESTATE.IDLE;
+
+        m_Seeker = GetComponent<Seeker>();
+        TargetPosition.parent = null;
+        InvokeRepeating("UpdatePath", 0.0f, 0.5f);
     }
 
-    void Update()
+    void UpdatePath()
+    {
+        if(m_Seeker.IsDone() && TargetPosition)
+        {
+            m_Seeker.StartPath(m_RigidBody.position, TargetPosition.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path _Path)
+    {
+        if (!_Path.error)
+        {
+            m_Path = _Path;
+            m_CurrentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
     {
         if (m_Animator)
         {
@@ -61,6 +96,27 @@ public class Script_CrewMate : MonoBehaviour
             }
         }
 
+        //Move character to TargetPosition
+        if (m_MateState == MATESTATE.ONROUTE && m_Path != null && TargetPosition)
+        {
+            Vector2 MovementDirection = ((Vector2)m_Path.vectorPath[m_CurrentWaypoint] - m_RigidBody.position).normalized;
+
+            //Horizontal Movement
+            Vector2 MovementVector = Vector2.zero;
+            MovementVector.x = Mathf.Sign(MovementDirection.x) * m_Speed;
+            
+            
+            m_RigidBody.velocity = MovementVector;
+            
+            float Distance = Vector2.Distance(transform.position, m_Path.vectorPath[m_CurrentWaypoint]);
+
+            if (Distance < m_NextWaypointDistance)
+            {
+                m_CurrentWaypoint++;
+            }
+        }
+        //Check if there is a wall on the player's side
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
 
     }
 }
