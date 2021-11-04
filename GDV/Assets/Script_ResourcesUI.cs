f1b601e1262004ac5f752e1b7cb4a03175e95505
Trandunz <trandunz@gmail.com>
2021-11-05 00:20:48 +1300

    + UI Improvements And Animations
     (TAB for hiding and showing UI elements in-game)

---
+++ GDV/Assets/Script_ResourcesUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_ResourcesUI : MonoBehaviour
+{
+    bool m_bShowUI = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            m_bShowUI = !m_bShowUI;
+
+            if (m_bShowUI)
+            {
+                GetComponent<Animator>().SetTrigger("Show");
+            }
+            else
+            {
+                GetComponent<Animator>().SetTrigger("Hide");
+            }
+        }
+    }
+}
