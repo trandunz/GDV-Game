b343d523f383d02d014db5b2d71bd643d4a5c9fb
MichaelWkTran <Michael.wktran@gmail.com>
2021-11-10 07:43:14 +1300

    Implemented CrewMate persistence

--- GDV/Assets/Scripts/CrewMate/Script_CrewMateManager.cs
+++ GDV/Assets/Scripts/CrewMate/Script_CrewMateManager.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 public class Script_CrewMateManager : MonoBehaviour
 {
     public GameObject[] m_CrewMates;
+    private GameObject m_GameManager;
 
     public Script_CrewMate m_activeCrewmate;
 
     void Start()
     {
         m_CrewMates = GameObject.FindGameObjectsWithTag("CrewMate");
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        //Called when a new level loads (Not tested)
+        if (m_GameManager == null)
+        {
+            m_GameManager = GameObject.FindGameObjectWithTag("GameManager");
+            
+            //Place all its crewmates at spawn position
+            foreach (GameObject CrewMate in m_CrewMates)
+            {
+                CrewMate.transform.position = m_GameManager.GetComponent<GameManager>().m_CrewmateSpawnPostion;
+            }
+        }
     }
 }
