20e8ca073f363dc780b211f4cd10aacf1b226445
Trandunz <trandunz@gmail.com>
2021-11-24 19:31:24 +1300

    + Small Fix To CrewmateUI

--- GDV/Assets/Scripts/GameManager.cs
+++ GDV/Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public int m_LevelIndex;
     public GameObject[] m_Levels;
+    public GameObject[] m_Stations;
 
     public Script_CrewMateManager m_CrewMateManager;
 
@@ -13,11 +14,17 @@ public class GameManager : MonoBehaviour
     {
         foreach (GameObject Level in m_Levels) { Level.SetActive(false); }
         m_Levels[m_LevelIndex].SetActive(true);
+
+        foreach (GameObject Level in m_Stations) { Level.SetActive(false); }
+        m_Stations[m_LevelIndex].SetActive(true);
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            UpgradeShip();
+        }
     }
 
     public void UpgradeShip()
@@ -27,6 +34,7 @@ public class GameManager : MonoBehaviour
             m_Levels[m_LevelIndex].SetActive(false);
             m_LevelIndex++;
             m_Levels[m_LevelIndex].SetActive(true);
+            m_Stations[m_LevelIndex].SetActive(true);
         }
     }
 }
