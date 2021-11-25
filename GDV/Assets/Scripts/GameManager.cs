6383c35e7f5002d345bf3799d3fb7106e567190b
Trandunz <trandunz@gmail.com>
2021-11-25 23:40:33 +1300

    + Patched Stations

--- GDV/Assets/Scripts/GameManager.cs
+++ GDV/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public int m_LevelIndex;
     public GameObject[] m_Levels;
+    public GameObject[] m_Stations;
 
     public Script_CrewMateManager m_CrewMateManager;
 
@@ -14,6 +15,9 @@ public class GameManager : MonoBehaviour
     {
         foreach (GameObject Level in m_Levels) { Level.SetActive(false); }
         m_Levels[m_LevelIndex].SetActive(true);
+
+        foreach (GameObject Level in m_Stations) { Level.SetActive(false); }
+        m_Stations[m_LevelIndex].SetActive(true);
     }
 
     void Update()
@@ -38,6 +42,7 @@ public class GameManager : MonoBehaviour
                 m_Levels[m_LevelIndex].SetActive(false);
                 m_LevelIndex++;
                 m_Levels[m_LevelIndex].SetActive(true);
+                m_Stations[m_LevelIndex].SetActive(true);
             }
         }
     }
