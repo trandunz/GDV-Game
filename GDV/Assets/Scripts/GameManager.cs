baac4fb44121965a3f5d1635a993cba5a1f04653
Trandunz <trandunz@gmail.com>
2021-11-25 21:58:09 +1300

    + WIn Condition And Ship Upgrades

--- GDV/Assets/Scripts/GameManager.cs
+++ GDV/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -25,11 +26,34 @@ public class GameManager : MonoBehaviour
 
     public void UpgradeShip()
     {
-        if (m_LevelIndex < m_Levels.Length)
+        if (GameObject.FindObjectOfType<Script_ResourcesUI>().m_Metal >= 200)
         {
-            m_Levels[m_LevelIndex].SetActive(false);
-            m_LevelIndex++;
-            m_Levels[m_LevelIndex].SetActive(true);
+            if (m_LevelIndex + 1 == m_Levels.Length)
+            {
+                WinCondition();
+            }
+            else if (m_LevelIndex + 1 < m_Levels.Length)
+            {
+                GameObject.FindObjectOfType<Script_ResourcesUI>().m_Metal -= 200;
+                m_Levels[m_LevelIndex].SetActive(false);
+                m_LevelIndex++;
+                m_Levels[m_LevelIndex].SetActive(true);
+            }
         }
+    }
+
+    void WinCondition()
+    {
+
+        StartCoroutine(WaitForTime(2)); // Wait Delay For Animation
+
+    }
+
+    IEnumerator WaitForTime(int _seconds)
+    {
+        // Some Animation
+        yield return new WaitForSeconds(_seconds);
+        // Some UI
+        SceneManager.LoadSceneAsync(0); // Main Menu
     }
 }
